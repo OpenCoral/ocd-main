49424885fd157810f51cda12205e72f50d527653
Jarkko <jm60697@gmail.com>
2012-01-13 15:23:24 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OcdMain::Application.config.secret_token = 'ade4d7752fb7931e7cda3d1e352e20cf82f11a24177ab2094c7f4ef2a4f18b64e5f40f9cf5a0d9fee5160ccb7fbc3fddfe05d1e78e535b61134fb961e1161577'
